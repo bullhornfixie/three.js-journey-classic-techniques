dbc8ec9ad2cbd6538a4e2265733adaa97cca2c7d
Jon Dawson <dawson.jon@hotmail.com>
2021-09-07 14:07:23 +0100

    added camera helper

--- 15-shadows/src/script.js
+++ 15-shadows/src/script.js
@@ -34,6 +34,13 @@ scene.add(directionalLight)
 
 directionalLight.castShadow = true
 
+directionalLight.shadow.mapSize.width = 1024 
+directionalLight.shadow.mapSize.height = 1024 
+console.log(directionalLight.shadow.camera)
+
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionalLightCameraHelper)
+
 /**
  * Materials
  */
