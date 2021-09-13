12ad989e84f7357c8f860e734d057f069ec8469d
Jon Dawson <dawson.jon@hotmail.com>
2021-09-13 08:53:25 +0100

    used for loop to create random colors

--- 17-particles/readme.md
+++ 17-particles/readme.md
@@ -4,8 +4,14 @@
 - Custom geometry using `BufferGeometry()`
 - `alphaTest` value between 0 and 1, enables WebGL to know when not to render the pixel - default the value is 0. 
 - `additiveBlending` color of two objects are added to each other - e.g. becomes brighter 
-- 
 
+### Creating Random Colors 
+- A color is composed of RGB
+- Using a `for loop` to create random colors 
+- R has a range of 0-1
+- G has a range of 0-1
+- B has a range of 0-1
+- When each element has a different range, it creates a different color
 
 ## Resources 
 [Particle Pack](https://www.kenney.nl/assets/particle-pack)
--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -29,15 +29,24 @@ const particlesGeometry = new THREE.BufferGeometry()
 const count = 20000 // 5000 particles
 
 const positions = new Float32Array(count * 3)
+const colors = new Float32Array(count * 3)
 
-for(let i = 0; i < count * 3; i++)
+for(let i = 0; i < count * 3; i++) 
 {
-    positions[i] = (Math.random() - 0.5) * 10
+  positions[i] = (Math.random() - 0.5) * 10
+  colors[i] = Math.random() 
 }
+// for loop is used for two different reasons - random verticies and colors 
 
 particlesGeometry.setAttribute(
   'position',
   new THREE.BufferAttribute(positions, 3)
+
+)
+
+particlesGeometry.setAttribute(
+  'color',
+  new THREE.BufferAttribute(colors, 3)
 )
 
 // Material 
@@ -50,7 +59,8 @@ const particlesMaterial = new THREE.PointsMaterial({
   // alphaTest: 0.001,
   // depthTest: false
   depthWrite: false,
-  blending: THREE.AdditiveBlending
+  blending: THREE.AdditiveBlending,
+  vertexColors: true
 })
 
 // Points 
