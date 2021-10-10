9037ccddc0f64b1fd1d3c20704de070a87230b93
Jon Dawson <dawson.jon@hotmail.com>
2021-10-10 06:52:45 +0100

    particles rendering on scene

---
+++ 18-galaxy-generator/src/script.js
@@ -0,0 +1,129 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+// Debug panel 
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Galaxy 
+const parameters = {}
+parameters.count = 1000
+parameters.size = 0.02
+// create 1000 particles 
+
+/** parameters is an object. This is a standalone entity with properties and type. Compare it with a cup, 
+ * a cup, is an object, with properties. A cup has a color, a design, weight, a material it is made of etc.
+ * The same way, JavaScript objects can have properties, which define their characteristics. 
+*/
+
+const generateGalaxy = () =>
+{
+  const geometry = new THREE.BufferGeometry()
+
+  const positions = new Float32Array(parameters.count * 3)
+  // * 3 vertiicies on each position 
+
+  for(let i = 0; i < parameters.count; i++) 
+  {
+    const i3 = i * 3
+
+    positions[i3 + 0] = Math.random() // x
+    positions[i3 + 1] = Math.random() // y
+    positions[i3 + 2] = Math.random() // z
+
+  }
+ 
+  geometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
+  )
+
+  // Material 
+  const material = new THREE.PointsMaterial({
+    size: parameters.size,
+    sizeAttenuation: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending
+  })
+
+  // Points
+  const points = new THREE.Points(geometry, material)
+  scene.add(points)
+
+}
+
+generateGalaxy()
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 3
+camera.position.y = 3
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
